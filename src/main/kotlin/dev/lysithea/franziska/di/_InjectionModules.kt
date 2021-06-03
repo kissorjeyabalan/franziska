a5e8e0f84e781cb2a649330d5634db747428132e
Kissor Jeyabalan <git@kij.no>
2021-06-03 16:41:13 +0200

    refactor(config): load config from root directory

--- src/main/kotlin/dev/lysithea/franziska/di/_InjectionModules.kt
+++ src/main/kotlin/dev/lysithea/franziska/di/_InjectionModules.kt
@@ -1,16 +1,24 @@
 package dev.lysithea.franziska.di
 
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.PropertySource
 import dev.lysithea.franziska.core.config.Config
 import dev.lysithea.franziska.core.database.DataService
 import dev.lysithea.franziska.core.database.MongoService
 import org.koin.core.Koin
 import org.koin.dsl.module
+import java.io.File
 
 /**
  * Modules for dependency injection with [Koin].
  */
 val franziskaModules = module {
-    single { ConfigLoader().loadConfigOrThrow<Config>("/config.yaml") }
+    single {
+        ConfigLoader.Builder()
+            .addSource(PropertySource.file(File("config.yaml"), true))
+            .addSource(PropertySource.file(File("../../config.yaml"), true))
+            .build()
+            .loadConfigOrThrow<Config>()
+    }
     single<DataService> { MongoService() }
 }
