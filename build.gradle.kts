bab17a1690b987f563351d47a2d0aa7376b1d87f
Kissor Jeyabalan <kij@kij.no>
2021-09-29 15:53:54 +0200

    chore: add initial project

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+
+    kotlin("jvm")
+
+    id("com.github.jakemarsden.git-hooks")
+    id("com.github.johnrengelman.shadow")
+    id("io.gitlab.arturbosch.detekt")
+}
+
+group = "dev.lysithea"
+version = "2.0.0"
+
+repositories {
+    maven {
+        name = "Kotlin Discord"
+        url = uri("https://maven.kotlindiscord.com/repository/maven-public/")
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt)
+
+    implementation(libs.kord.extensions)
+    implementation(libs.kotlin.stdlib)
+
+    // Logging dependencies
+    implementation(libs.groovy)
+    implementation(libs.logback)
+    implementation(libs.logging)
+}
+
+application {
+    // This is deprecated, but the Shadow plugin requires it
+    mainClassName = "dev.lysithea.franziska.AppKt"
+}
+
+gitHooks {
+    setHooks(
+        mapOf("pre-commit" to "detekt")
+    )
+}
+
+tasks.withType<KotlinCompile> {
+    // Current LTS version of Java
+    kotlinOptions.jvmTarget = "11"
+
+    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "dev.lysithea.franziska.AppKt"
+        )
+    }
+}
+
+java {
+    // Current LTS version of Java
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config = rootProject.files("detekt.yml")
+}
---
+++ src/main/kotlin/dev/lysithea/franziska/App.kt
@@ -0,0 +1,5 @@
+package dev.lysithea.franziska
+
+suspend fun main() {
+
+}
